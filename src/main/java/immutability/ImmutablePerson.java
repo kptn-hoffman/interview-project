f560aed0f35cba30c50c1b8ebfe38cd4d4017de7
Nicolae Nicolaev <Nicolae.Nicolaev@endava.com>
2022-03-04 14:55:59 +0200

    added immutability example class

---
+++ src/main/java/immutability/ImmutablePerson.java
@@ -0,0 +1,42 @@
+package immutability;
+
+public class ImmutablePerson {
+
+    private Integer id;
+    private String firstName;
+    private String lastName;
+
+    public ImmutablePerson() {
+
+    }
+
+    public ImmutablePerson(Integer id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
